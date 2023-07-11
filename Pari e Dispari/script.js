ed3c66e174d430b4268959b7adc222089b3712bf
PasqualeIsabelli <pasquale0395@gmail.com>
2023-07-11 14:04:20 +0200

    fix pari e dispari

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -7,28 +7,47 @@
   5. Dichiariamo chi ha vinto.
 */
 
+// Chiedo all'utente di scegliere tra pari o dispari
+const evenOdd = prompt("Scegli pari o dispari");
+console.log(evenOdd);
 
 // Chiedo all'utente di inserire un numero da 1 a 5
 const userNumber = parseInt(prompt("Inserisci un numero da 1 a 5"));
 console.log(userNumber);
 
-// Genero un numero random da 1 a 5 per il pc
-let computerNumber = Math.floor(Math.random() * 5) + 1;
-console.log(computerNumber)
+// Invoco la funzione del numero del pc
+getRandomNum();
 
-// Invoco la funzione
-sumNumbers(userNumber, computerNumber)
+// Funzione numero pc
+function getRandomNum() {
+  // Genero un numero random da 1 a 5 per il pc
+  let computerNumber = Math.floor(Math.random() * 5) + 1;
+  console.log(computerNumber);
+  return computerNumber;
+}
+
+// Invoco la funzione della somma dei due numeri
+let sum = sumNumbers();
 
 // Funzione
-function sumNumbers(userNum, pcNum) {
+function sumNumbers(userNumber, computerNumber) {
   // Sommo i due numeri
-  let sum = userNum + pcNum;
-  console.log(sum);
+  const sum = userNumber + computerNumber;
 
   // Stabilisco la condizione (se la somma è pari vince l'utente altrimenti vince il pc)
   if (sum % 2 === 0) {
-    console.log ("Utente ha vinto");
+    console.log("La somma è pari");
   } else {
-    console.log ("Pc ha vinto")
+    console.log("La somma è dispari")
   }
+
+  if (sum % 2 === 0 && evenOdd === "pari") {
+    console.log("Utente ha vinto");
+  } else if (sum % 2 !== 0 && evenOdd === "dispari") {
+    console.log("Utente ha vinto");
+  } else {
+    console.log("Pc ha vinto");
+  }
+
+  return sum;
 }
